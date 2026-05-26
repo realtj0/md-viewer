b187f82a46b1ad31ac9891983ffdb473ca4fb83f
realtj0 <thomas@tjaconsulting.fr>
2026-05-26 12:42:07 +0200

    Convert to PWA: manifest, service worker, theme-color meta

---
+++ sw.js
@@ -0,0 +1,41 @@
+const CACHE = 'mdviewer-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './marked.min.js',
+  './purify.min.js',
+  './favicon.svg',
+  './manifest.webmanifest'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE)
+      .then(cache => cache.addAll(ASSETS))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(
+    caches.match(event.request).then(cached => {
+      const fetchPromise = fetch(event.request).then(response => {
+        if (response && response.ok && response.type === 'basic') {
+          const copy = response.clone();
+          caches.open(CACHE).then(cache => cache.put(event.request, copy));
+        }
+        return response;
+      }).catch(() => cached);
+      return cached || fetchPromise;
+    })
+  );
+});
